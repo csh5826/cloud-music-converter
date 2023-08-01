e437b562c331fbc3d26d47eb9e1cf5235b8bc452
csh5826 <63155575+csh5826@users.noreply.github.com>
2023-08-01 16:27:22 +0000

    cleaning up query to spotify

--- main.py
+++ main.py
@@ -6,27 +6,21 @@ USER_ID = 'br18lp7xinjotspsyry15qyrk'
 PLAYLIST_NAME='all songs from apple'
 redirect_uri = 'https://localhost:8888/callback'
 
-def get_spotify_id(song_title, spotipy_client): #make this plural and send in array first
-    params = {
-        'q': f'track:{song_title}',
-        'type': 'track',
-        'limit': 1
-    }
-    data = spotipy_client.search(params)
+def get_spotify_uri(title, artist): #make this plural and send in array first
+    query = f'track:{title} artist:{artist}'
+    data = spotify.search(query, limit=1)
 
     if 'tracks' in data and 'items' in data['tracks'] and data['tracks']['items']:
         track = data['tracks']['items'][0]
-        return track['uri']
-    else:
+        return track['uri'] #ideally we write this value to the csv
+    else: #need to update this logic to note we weren't able to get that track (ideally we are writing to the csv here)
         return None
 
-def add_tracks_to_playlist(track_uris, spotify_client): #need to. figure out if we need the track ids or what here
+def add_tracks_to_playlist(track_uris, spotify_client): # need a list of track URIs
     spotify_client.playlist_add_items(PLAYLIST_ID, track_uris)
 
-
-# Replace the list below with your array of song titles
-song_titles = ['Cries in vein', 'Lemonade', 'Trapper']
-
+#list of dictionaries for artist and song
+apple_music_metadata = [{'artist': 'zach bryan', 'title': 'oklahoma smokeshow'}]
 
 spotify = spotipy_oauth()
 
@@ -34,22 +28,19 @@ spotify = spotipy_oauth()
 PLAYLIST_ID = '47VW4a6XXezcUi9Ev2MZiP'
 
 # playlist_id = create_playlist(api_token, user_id, PLAYLIST_NAME)
-# print('playlist_id is')
-# print(playlist_id)
 
 # Store the results in an array
 results = []
 
-for song_title in song_titles: #only adding the first track for some reason
-    spotify_id = get_spotify_id(song_title, spotify)
+for metadata in apple_music_metadata:
+    spotify_id = get_spotify_uri(metadata['title'], metadata['artist'])
     if spotify_id:
         results.append(spotify_id)
 
 print("Spotify URI's of the songs:")
 print(results)
 
-test = add_tracks_to_playlist(results, spotify)
-print(test)
+add_tracks_to_playlist(results, spotify)
 
 
 # spotify_uris = ['4RSpef8XeBACK6MNkfdVE7', '7hxHWCCAIIxFLCzvDgnQHX', '4XaW4jIud5YxwV4QRJOb40']
