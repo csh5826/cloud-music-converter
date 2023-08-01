40a7a1a8e41e0517d845742b52b903cdd30dc2c1
csh5826 <63155575+csh5826@users.noreply.github.com>
2023-08-01 15:26:21 +0000

    leveraging spotipy over custom code, clean up

--- main.py
+++ main.py
@@ -1,78 +1,55 @@
 import requests
 from urllib.parse import urlencode
-from auth import get_api_token, get_user_token, create_spotify_oauth
+from auth import spotipy_oauth
 
-# Replace 'USER_ID' with the user ID of your Spotify account
-# USER_ID='' #need this
-# Replace 'AUTHORIZATION_CODE' with the authorization code you obtain after the user logs in and grants access
-# AUTHORIZATION_CODE = 'YOUR_AUTHORIZATION_CODE'
-
-user_id = 'YOUR_USER_ID'
+USER_ID = 'br18lp7xinjotspsyry15qyrk'
 PLAYLIST_NAME='all songs from apple'
+redirect_uri = 'https://localhost:8888/callback'
 
-def get_spotify_id(song_title, access_token):
-    base_url = 'https://api.spotify.com/v1/search'
-    headers = {'Authorization': 'Bearer ' + access_token}
+def get_spotify_id(song_title, spotipy_client): #make this plural and send in array first
     params = {
         'q': f'track:{song_title}',
         'type': 'track',
         'limit': 1
     }
-
-    response = requests.get(base_url, headers=headers, params=params)
-    data = response.json()
-    print(data)
+    data = spotipy_client.search(params)
 
     if 'tracks' in data and 'items' in data['tracks'] and data['tracks']['items']:
         track = data['tracks']['items'][0]
-        return track['id']
+        return track['uri']
     else:
         return None
 
-def create_playlist(access_token, user_id, playlist_name):
-    create_playlist_url = f'https://api.spotify.com/v1/users/{user_id}/playlists'
-    headers = {'Authorization': 'Bearer ' + access_token, 'Content-Type': 'application/json'}
-    data = {'name': playlist_name, 'public': False}
-
-    response = requests.post(create_playlist_url, headers=headers, json=data)
-    playlist_id = response.json().get('id')
-
-    return playlist_id
-
-def add_tracks_to_playlist(access_token, playlist_id, track_uris):
-    add_tracks_url = f'https://api.spotify.com/v1/playlists/{playlist_id}/tracks'
-    headers = {'Authorization': 'Bearer ' + access_token, 'Content-Type': 'application/json'}
-    data = {'uris': track_uris}
-
-    response = requests.post(add_tracks_url, headers=headers, json=data)
-
-    return response.status_code == 201
+def add_tracks_to_playlist(track_uris, spotify_client): #need to. figure out if we need the track ids or what here
+    spotify_client.playlist_add_items(PLAYLIST_ID, track_uris)
 
 
 # Replace the list below with your array of song titles
 song_titles = ['Cries in vein', 'Lemonade', 'Trapper']
 
 
-#AUTH 
-token = create_spotify_oauth()
-api_token = get_api_token(token)
-user_token = get_user_token() #need arg here
+spotify = spotipy_oauth()
 
+# created_playlist = spotify.user_playlist_create(USER_ID, PLAYLIST_NAME, True, False, '')
+PLAYLIST_ID = '47VW4a6XXezcUi9Ev2MZiP'
 
-playlist_id = create_playlist(user_token, user_id, PLAYLIST_NAME)
-print('playlist_id is')
-print(playlist_id)
+# playlist_id = create_playlist(api_token, user_id, PLAYLIST_NAME)
+# print('playlist_id is')
+# print(playlist_id)
 
 # Store the results in an array
 results = []
 
-for song_title in song_titles:
-    spotify_id = get_spotify_id(song_title, api_token)
+for song_title in song_titles: #only adding the first track for some reason
+    spotify_id = get_spotify_id(song_title, spotify)
     if spotify_id:
         results.append(spotify_id)
 
-print("Spotify IDs of the songs:")
+print("Spotify URI's of the songs:")
 print(results)
 
+test = add_tracks_to_playlist(results, spotify)
+print(test)
 
-spotify_ids = ['4RSpef8XeBACK6MNkfdVE7', '7hxHWCCAIIxFLCzvDgnQHX', '4XaW4jIud5YxwV4QRJOb40']
+
+# spotify_uris = ['4RSpef8XeBACK6MNkfdVE7', '7hxHWCCAIIxFLCzvDgnQHX', '4XaW4jIud5YxwV4QRJOb40']
