08afe8fafac216d8e3aea025435ea08a73bc396d
csh5826 <63155575+csh5826@users.noreply.github.com>
2023-08-02 17:35:05 +0000

    adding ability to pass in an excel file and create a playlist for each sheet and add all songs associated

--- excel_helper.py
+++ excel_helper.py
@@ -4,23 +4,24 @@ import os
 # og_path = './data/all songs from apple.xlsx'
 #updated_path = './data/all songs with uri.xlsx'
 
-def extract_title_and_artist_from_excel(path):
+def extract_title_and_artist_from_excel(path, sheet_name):
     data_list = []
     wb = openpyxl.load_workbook(path)
-    sheet = wb.active
+    sheet = wb[sheet_name]
 
     # Assuming column 1 is 'title' and column 3 is 'artist'
     for row in sheet.iter_rows(min_row=2, values_only=True):
         title, _, artist, *_ = row  # Extract title and artist from the row
-        data_list.append({'title': title, 'artist': artist})
+        if title != None:
+            data_list.append({'title': title, 'artist': artist})
 
     wb.close()
     return data_list
 
-def extract_uid_from_excel(path):
+def extract_uid_from_excel(path, sheet_name):
     uid_list = []
     wb = openpyxl.load_workbook(path)
-    sheet = wb.active
+    sheet = wb[sheet_name]
 
     for row in sheet.iter_rows(min_row=2, values_only=True):
         spotify_uid = row[5]  # Extract title and artist from the row
@@ -30,9 +31,9 @@ def extract_uid_from_excel(path):
     wb.close()
     return uid_list
 
-def write_spotify_uid_to_excel(uid_list, path):
-    wb = openpyxl.load_workbook(path)
-    sheet = wb.active
+def write_spotify_uid_to_excel(uid_list, path, sheet_name):
+    wb = openpyxl.load_workbook(path)                           
+    sheet = wb[sheet_name]
 
     # Start writing from the second row (skipping the header row)
     for i, spotify_uid in enumerate(uid_list, start=2):
@@ -40,36 +41,6 @@ def write_spotify_uid_to_excel(uid_list, path):
 
     wb.save(path)
     wb.close()
-
-# def create_excel_sheet(name, columns):
-#     workbook = openpyxl.Workbook()
-#     sheet = workbook.active
-#     sheet.title = name
-#     # Write column headers to the sheet
-#     for col_idx, column in enumerate(columns, start=1):
-#         sheet.cell(row=1, column=col_idx, value=column)
-#     # Save the workbook in the 'data' folder
-#     data_folder = 'data'
-#     os.makedirs(data_folder, exist_ok=True)
-#     file_path = os.path.join(data_folder, f"{name}.xlsx")
-#     workbook.save(file_path)
-#     return file_path
-
-# def copy_sheet_data(source_path, new_path): #source_sheet_name <-- may want to utilize this
-#     source_wb = openpyxl.load_workbook(source_path)
-#     source_sheet = source_wb.active
-
-#     # Create or load the destination workbook and sheet
-#     try:
-#         destination_workbook = openpyxl.load_workbook(new_path)
-#     except FileNotFoundError:
-#         destination_workbook = openpyxl.Workbook()
-#         destination_sheet = destination_workbook.create_sheet()
-
-#     for row in source_sheet.iter_rows(values_only=True):
-#         destination_sheet.append(row)
-
-#     destination_workbook.save(new_path)
 
 def load_copy_create_helper(input_file, output_file): ##look into adding this for line 87 func
     # Load the workbook and active sheet from the input file
@@ -84,36 +55,48 @@ def load_copy_create_helper(input_file, output_file): ##look into adding this fo
     header_row = next(input_sheet.iter_rows(values_only=True))
     output_sheet.append(header_row)
 
-def remove_rows_without_spotify_uid(input_file, output_file):
+def remove_rows_without_spotify_uid(input_file, output_file, sheet_name):
     # Load the workbook and active sheet from the input file
     input_workbook = openpyxl.load_workbook(input_file)
-    input_sheet = input_workbook.active #
+    input_sheet = input_workbook[sheet_name] #CHANGE LOGIC HERE TO SHEET NAME
 
     # Create a new workbook and sheet for the filtered data
     output_workbook = openpyxl.Workbook()
-    output_sheet = output_workbook.active
+    try:
+        output_sheet = output_workbook[sheet_name]
+    except KeyError:
+        output_sheet = output_workbook.create_sheet(sheet_name) 
 
     # Copy the header row to the output sheet
     header_row = next(input_sheet.iter_rows(values_only=True))
     output_sheet.append(header_row)
+    valid_rows = []
 
-    # Copy only the rows with a value of 'NOT FOUND' in the Spotify UID column
+    # Copy only the rows with a value of 'NOT FOUND' in the Spotify UID column else append a valid row to 
     for row in input_sheet.iter_rows(min_row=2, values_only=True):
         spotify_uid = row[5]  # Assuming column 6 is the 'Spotify UID' column
         if spotify_uid == 'NOT FOUND':
             output_sheet.append(row)
+        else:
+            valid_rows.append(row)
+    #Delete all rows except top row from original
+    input_sheet.delete_rows(idx=2, amount=input_sheet.max_row - 1)
+    #put back in valid rows
+    for row in valid_rows:
+        input_sheet.append(row)
 
-    # Save the new workbook to the output file
+    # Save workbooks to file path provided
     output_workbook.save(output_file)
+    input_workbook.save(input_file)
 
-def remove_parentheses_from_column(input_file, output_file, column_index):
+def remove_parentheses_from_column(input_file, output_file, column_index, sheet_name):
     # Load the workbook and active sheet from the input file
     input_workbook = openpyxl.load_workbook(input_file)
-    input_sheet = input_workbook.active
+    input_sheet = input_workbook[sheet_name] 
 
     # Create a new workbook and sheet for the modified data
     output_workbook = openpyxl.Workbook()
-    output_sheet = output_workbook.active
+    output_sheet = output_workbook[sheet_name] 
 
     # Copy the header row to the output sheet
     header_row = next(input_sheet.iter_rows(values_only=True))
@@ -132,3 +115,7 @@ def remove_parentheses_from_column(input_file, output_file, column_index):
 
     # Save the new workbook to the output file
     output_workbook.save(output_file)
+
+def get_sheet_names(path):
+    workbook = openpyxl.load_workbook(path)
+    return workbook.sheetnames
--- spotify.py
+++ spotify.py
@@ -1,68 +1,92 @@
 from auth import spotipy_oauth
-from excel_helper import extract_title_and_artist_from_excel, write_spotify_uid_to_excel, extract_uid_from_excel, remove_parentheses_from_column
+from excel_helper import extract_title_and_artist_from_excel, write_spotify_uid_to_excel, get_sheet_names, remove_rows_without_spotify_uid, extract_uid_from_excel
 import time
+import os
+from dotenv import load_dotenv
+# PROBABLY WORTH MAKING EXCEL_HELPER INTO ITS OWN CLASS
+load_dotenv()
 
-USER_ID = 'br18lp7xinjotspsyry15qyrk'
+USER_ID = os.getenv("USER_ID")
 PLAYLIST_NAME='all songs from apple'
 og_path = './data/all songs from apple.xlsx'
 updated_path = './data/all songs with uri.xlsx'
 not_found_path = './data/all songs with not found uri.xlsx'
 not_found_no_title_parentheses = './data/all songs not found no title parentheses.xlsx'
 
-def get_spotify_uri(title, artist): #make this plural and send in array first
+def create_playlists(playlist_names):
+    playlist_metadata = []
+    for name in playlist_names:
+        id = spotify.user_playlist_create(USER_ID, name, True, False, '')['id']
+        metadata = {'name': name, 'id': id}
+        playlist_metadata.append(metadata)
+    return playlist_metadata
+
+def get_spotify_uri(title, artist):
     query = f'track:{title} artist:{artist}'
     data = spotify.search(query, limit=1)
 
     if 'tracks' in data and 'items' in data['tracks'] and data['tracks']['items']:
         track = data['tracks']['items'][0]
-        return track['uri'] #ideally we write this value to the csv
-    else: #lets return 'NOT FOUND'
+        return track['uri']
+    else:
         return 'NOT FOUND'
 
-def add_tracks_to_playlist(track_uris): # need a list of track URIs
+def add_tracks_to_playlist(track_uris, playlist_id): # need a list of track URIs
     print('Processing these tracks:', track_uris)
-    spotify.playlist_add_items(PLAYLIST_ID, track_uris)
+    spotify.playlist_add_items(playlist_id, track_uris) 
     return f'Your playlist: {PLAYLIST_NAME} has been updated'
 
 # created_playlist = spotify.user_playlist_create(USER_ID, PLAYLIST_NAME, True, False, '')
 PLAYLIST_ID = '47VW4a6XXezcUi9Ev2MZiP'
 
-def grab_uids_and_append_to_excel(music_metadata, path): #look into speeding this up somehow 
+def grab_uids_and_append_to_excel(music_metadata, path, sheet_name): #look into speeding this up somehow 
     results = []
-    for metadata in music_metadata: #apple_music_metadata
+    for metadata in music_metadata:
         spotify_uri = get_spotify_uri(metadata['title'], metadata['artist'])
         print('spotify uri for:', metadata['title'])
         print(spotify_uri)
-        #if spotify_id is not 'NOT FOUND':
         results.append(spotify_uri)
-        time.sleep(60/175) #adding this bc spotify has governor limits of around 180 requests per minute (this will be about 175)
-   #write the spotify id to excel here
-    write_spotify_uid_to_excel(results, path)
+        time.sleep(60/175)
+    write_spotify_uid_to_excel(results, path, sheet_name) #update this
     try:
         while True:
             results.remove('NOT FOUND')
     except ValueError:
         pass
 
-def register_songs_to_playlist(uid_list):
+def register_songs_to_playlist(uid_list, playlist_id):
   for i in range(0, len(uid_list), 75): #max amount of uris in one request is 100
       batch = uid_list[i:i + 75]
-      add_tracks_to_playlist(batch)
+      add_tracks_to_playlist(batch, playlist_id)
       time.sleep(60/175)
 
 
 spotify = spotipy_oauth()
+my_playlist_names = get_sheet_names(og_path)
+my_playlist_names.pop(0)
+my_playlist_names = ['firework']
+
+playlist_metadata = create_playlists(my_playlist_names)
+# FOR LOOP HERE 
+for entry in playlist_metadata:
+    song_metadata = extract_title_and_artist_from_excel(og_path, entry['name'])
+    print(song_metadata)
+    grab_uids_and_append_to_excel(song_metadata, og_path, entry['name'])
+    remove_rows_without_spotify_uid(og_path, not_found_path, entry['name'])
+    uids = extract_uid_from_excel(og_path, entry['name'])
+    register_songs_to_playlist(uids, entry['id'])
+
 #list of dictionaries for artist and song
 #apple_music_metadata = extract_title_and_artist_from_excel(og_path)
 #quick note, spotify does not like feat.
 # remove_parentheses_from_column(not_found_path, not_found_no_title_parentheses, 1)
-not_found_metadata = extract_title_and_artist_from_excel(not_found_path)
+# not_found_metadata = extract_title_and_artist_from_excel(not_found_path)
 # print(not_found_metadata)
 # grab_uids_and_append_to_excel(not_found_metadata, not_found_path)
 
 # uid_list = extract_uid_from_excel(updated_path)
 
-uid_list = extract_uid_from_excel(not_found_path)
-print(uid_list)
+# uid_list = extract_uid_from_excel(not_found_path)
+# print(uid_list)
 
 # register_songs_to_playlist(uid_list)
