809add74be3bac651211f0215044e7f5aa664ebf
csh5826 <63155575+csh5826@users.noreply.github.com>
2023-08-02 15:36:46 +0000

    updating songs still not found file

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-from excel_helper import handle_not_found_uids, remove_rows_without_spotify_uid
+from excel_helper import remove_rows_without_spotify_uid
 
 new_excel = './data/all songs with not found uri.xlsx'
 # columns = ['Title', 'Time', 'Artist', 'Album', 'Genre', 'Spotify URI']
-existing_excel = './data/all songs with uri.xlsx'
+existing_excel = './data/all songs not found no title parentheses.xlsx'
 # handle_not_found_uids(excel_name, columns, existing_excel)
 # def start():
 
